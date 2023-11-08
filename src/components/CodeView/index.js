c243ae9969f75b1117f7b881d84ae94337d62676
nvtngan <nvtngan@hcmus.edu.vn>
2023-11-07 22:03:51 -0600

    Update index.js

--- src/components/CodeView/index.js
+++ src/components/CodeView/index.js
@@ -34,8 +34,8 @@ parent(X,Y) :- mother(X,Y).
 
 child(X,Y) :- parent(Y, X).
 
-ancestor(X,Y) :- parent(X, Y),
-ancestor(X,Y) :- parent(Z, Y), ancestor(X,Z)`
+ancestor(X,Y) :- parent(X, Y).
+ancestor(X,Y) :- parent(Z, Y), ancestor(X,Z).`
 const script = [
     {label:'parent(X,Y)',onHover:[26,27]},
     {label:'child(X,Y)',onHover:[29]},
@@ -45,16 +45,62 @@ export default function() {
     const [codeline,setCodeline] = useState([]);
     const [codeHighlight,setCodeHighlight] = useState({});
     useEffect(()=>{
-        setCodeline(codeData.split('\n'));
+        // analyze
+        const lines = codeData.split('\n')
+            .map((text,id)=>{
+                const item = {id, text};
+                const _text = text.trim();
+                if (_text.match(/^%/))
+                    item.type = 'commment';
+                else if (text===''){
+                    item.type = 'blank';
+                }else{
+                    const splitRule = text.split(':-');
+                    if (splitRule.length>1) {
+                        item.type = 'rule';
+                        item.label = splitRule[0].trim();
+                        item.condition = [];
+                        let count = 0;
+                        splitRule[1].replace('.','').split(/([(,)])/).forEach(s=>{
+
+                        })
+                        splitRule[1].replace('.','').split(/([()])/).map(d=>d.trim());
+                    }else {
+                        const splitDefine = text.split('(');
+                        if (splitDefine.length > 1) {
+                            item.type = 'fact';
+                            item.label = splitDefine[0].trim();
+                            item.atom = text.split(`${splitDefine[0]}(`)[1].replace('.','').replace(')','').split(',').map(d => d.trim());
+                        }
+                    }
+                }
+                return item;
+            })
+        console.log(lines)
+        setCodeline(lines);
     },[])
     const highlight = useCallback((list)=>{
         return ()=>{
             const hlist = {};
             list.forEach(d=>hlist[d]=1);
             setCodeHighlight(hlist);
-            console.log(hlist)
         }
     },[codeline,script])
+    const generateTree = useCallback((label)=>{
+        return ()=>{
+
+            // replace special symbol
+            const _label = label.replace('(',"\\(").replace(')',"\\)")
+            // find rule
+            const rules = [];
+            codeline.forEach((d,i)=> {
+                if (d.match(`${_label} :-`))
+                    rules.push([i,d])
+            });
+            debugger
+            console.log(rules)
+        }
+    },[codeline])
     return <Grid container sx={{height:'calc(100% - 66px)',overflow:'hidden'}}>
         <Grid item xs={3} sx={{borderRight:1,padding:1}} className={"shadow"}>
             <Stack spacing={1}>
@@ -62,19 +108,20 @@ export default function() {
                 label={s.label} 
                 onMouseEnter={highlight(s.onHover??[])}
                 onMouseLeave={highlight([])}
+                onClick={generateTree(s.label)}
                 clickable/>)}
             </Stack>
         </Grid>
         <Grid item xs={9} sx={{padding:1,overflow:'auto',height:'100%'}}>
             <Stack sx={{display:'table', width:'100%'}} spacing={1}>
-                {codeline.map((d,i)=><Box key={i}
+                {codeline.map(({text,id})=><Box key={id}
                 sx={{display:'table-row', width:'auto', clear:'both', position:"relative"}}
                 >
                     <Box sx={{position:'absolute',width:'100%',height:'100%',opacity:0.2, borderRadius:1}}
-                    bgcolor={(theme)=>codeHighlight[i]?theme.palette.secondary.light:null}/>
+                    bgcolor={(theme)=>codeHighlight[id]?theme.palette.secondary.light:null}/>
                     <Box sx={{float:'left', display:'table-column', width:50, textAlign:'right',
-                        userSelect:'none',paddingRight:1,borderRight:1}}>{i}</Box>
-                    <Box sx={{float:'left', display:'table-column', width:'auto',paddingLeft:1}}>{d}</Box>
+                        userSelect:'none',paddingRight:1,borderRight:1}}>{id}</Box>
+                    <Box sx={{float:'left', display:'table-column', width:'auto',paddingLeft:1}}>{text}</Box>
                 </Box>)}
             </Stack>
         </Grid>
